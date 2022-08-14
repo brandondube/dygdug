7317eb9b121f77b5d46da5fbb66150c822817204
Brandon <brandondube@gmail.com>
2022-08-13 18:53:58 -0700

    py init

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""dygdug, dig that dark hole."""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(use_scm_version=True)
