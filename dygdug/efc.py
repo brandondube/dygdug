5087c04394e4bc565880314a81c0d7970881d75d
Brandon <brandondube@gmail.com>
2022-08-18 22:00:38 -0700

    models & efc: new EFC calling formalism, new model decomposition, some utils

--- dygdug/efc.py
+++ dygdug/efc.py
@@ -3,55 +3,198 @@ from tqdm import tqdm
 from prysm.mathops import np
 from prysm.conf import config
 
-def calculate_jacobian_fwddiff(lc, h=0.1, wvl=None):
+
+def calculate_jacobian_fwddiff(query_actuators, set_actuators, current_E_fields, h=0.1):
     """Calculate the Jacobian via forward differences.
 
     As opposed to central differences
 
     Parameters
     ----------
-    lc : dygdug.models.LyotCoronagraphSingleDM
-        the coronagraph model
+    query_actuators : callable
+        a function that when called returns all actuators in a system,
+        packed into a single vector
+    set_actuators : callable
+        a function that when called with a vector of all actuators in a system,
+        updates the state of the system
+    current_E_fields : callable
+        a function which returns one or more (mxn) shaped E-fields, where
+        'one or more' refers to discrete wavelengths or sub-bands
     h : float
-        step size for forward differences
-    wvl : float
-        wavelength of light, microns
+        step size in forward differences, same units as those expected by
+        set_actuators e.g. nanometers of surface figure, or voltage
 
     Returns
     -------
     numpy.ndarray
-        npix x nact array of complex forward differences
+        nwvl x (mpix x npix) x nact forward sensivity matrix
 
     """
-    acts = lc.dm.actuators.copy()
-    shp = acts.shape
-    nact = shp[0]*shp[1]
-    indices = np.arange(nact)
-    iy, ix = np.unravel_index(indices, shp)
-    # calculate resting E-field
-
-    # allocate the memory for the forward differences
-    E0 = lc.fwd(wvl).data
+    acts = query_actuators().copy()
+    nact = len(acts)
+    # dearest reader, you will see np.array called on E-fields in this function
+    # this is because the user may return a list or tuple of E-fields, which
+    # we can't get the shape of, and do not nicely assign into a block of another
+    # array for all numpy-like libraries.  If the user gives us an array, np.array()
+    # does nothing.  If they give us a list or a tuple, it costs almost nothing
+    # because numpy will make a non-contiguous array and it will Just Work TM
+    E0 = np.array(current_E_fields())
     jac = np.empty((*E0.shape, nact), dtype=complex)
     for i in tqdm(range(nact)):
-        iiy, iix = iy[i], ix[i]
         # poke
-        acts[iiy, iix] += h
-        lc.update_dm(acts)
-        Eup = lc.fwd(wvl).data
+        acts[i] += h
+        set_actuators(acts)
+        Eup = np.array(current_E_fields())
         D = (Eup-E0)/h
         jac[..., i] = D
         # restore
-        acts[iiy, iix] -= h
+        acts[i] -= h
 
     # final update to make sure last un-poke happens
-    lc.update_dm(acts)
+    set_actuators(acts)
     return jac
 
 
 class EFC:
     """Electric Field Conjugation."""
-    def __init__(self, G, c, beta, dh_mask, loop_gain=0.5):
+    def __init__(self, G, beta, dh_mask,
+                 query_actuators, set_actuators, current_E_fields,
+                 spectral_weights=None, loop_gain=0.5):
+        """Create a new Electric Field Conjugation instance.
+
+        Parameters
+        ----------
+        G : numpy.ndarray
+            (NxM)xK array, where (NxM) is the image area, without mask, and K
+            is the total number of actuators
+        beta : float
+            log10 of the corner of the Tikhonov window; typically values of
+            -1.5 to -2.5 result in "relaxed" control that is stable,
+            while results in the regime of -5 or smaller result in hyper
+            aggressive control that is likely divergent if sustained
+            use regularize_control_matrix(new_beta) to perform beta kicking
+            and similar operations
+        dh_mask : numpy.ndarray
+            mask which selects and optionally weights the dark hole;
+            pixels which are of value zero are excluded from control
+        query_actuators : callable
+            a function that when called returns all actuators in a system,
+            packed into a single vector
+        set_actuators : callable
+            a function that when called with a vector of all actuators in a system,
+            updates the state of the system
+        current_E_fields : callable
+            a function which returns one or more (mxn) shaped E-fields, where
+            'one or more' refers to discrete wavelengths or sub-bands
+        spectral_weights : numpy.ndarray
+            weights applied to each control sub-band, default is no weighting
+        loop_gain : float
+            the gain used.  A gain of one half is equivalent to steepest descent
+            while a gain of two is equal to steepest descent with a double step
+            algorithm;  see praise/SteepestDescent for similar language and
+            Fienup1993 for further discussion
+
+        """
+        self.Graw = G
+        self.beta = beta
+        self.dh_mask = dh_mask
+        if dh_mask.dtype == bool:
+            self.binary_dh_mask = dh_mask
+            self.need_spatial_weighting = False
+        else:
+            self.binary_dh_mask = dh_mask != 0
+            self.need_spatial_weighting = True
+
+        self.spectral_weights = spectral_weights  # TODO: implement this...
+
+        # the three functions that let EFC interact with the world
+        self.query_actuators = query_actuators
+        self.set_actuators = set_actuators
+        self.current_E_fields = current_E_fields
+
+        self.loop_gain = loop_gain
+
+        self.modify_jacobian_matrix()
+        self.compute_control_matrix()
+        self.act_cmds = query_actuators()
+        self.iter = 0
+
+    def modify_jacobian_matrix(self):
+        # ellipsis; if there is a spectral dimension on the front, skip over it
+        # and if not, select all wavelengths
+        if self.need_spatial_weighting:
+            # ellipsis, :, :, newaxis = elementwise product on spatial axes only
+            G = self.Graw * self.dh_mask[..., :, :, np.newaxis]
+        else:
+            G = self.Graw
+        G2 = G[..., self.binary_dh_mask, :]
+        # collapse all spatial and spectral dimensions, making an MxN matrix
+        # where M ~= number of pixels and N = number of actuators
+        G2 = G2.reshape((-1, G2.shape[-1]))
+        M, N = G2.shape
+        self.M = M
+        self.N = N
+        # now break out real and imag
+        G3 = np.empty((2*M, N), dtype=config.precision)
+        G3[:M] = G2.real
+        G3[M:] = G2.imag
+        self.Gmod = G3
+        self.Ework = np.zeros(2*M, dtype=config.precision)
+        return G3
+
+    def compute_control_matrix(self):
+        self.U, self.S, self.Vt = np.linalg.svd(self.Gmod, full_matrices=False)
+        self.regularize_control_matrix(self.beta)
+        return self.Gstar
+
+    def regularize_control_matrix(self, beta):
+        self.beta = beta
+        U, S, Vt = self.U, self.S, self.Vt
+        # DOI 10.1117/12.2274687
+        # Eq. 4
+        # delta h = V * diag ( ... ) * U^* * E
+        # ... =
+        #          s_i^2               1
+        # ------------------------ *  ---
+        # s_i^2 + s_1^2 * 10^\beta    s_i
+        #
+        # =
+        #           s_i
+        # ------------------------
+        # s_1^2 * 10^\beta + s_i^2
+        S1 = S[0]
+        denom = S1*S1 * 10 ** beta + S*S
+        Sinv = S / denom
+
+        # Gstar is the regularized pseudo-inverse
+        # of G
+        self.Gstar = (Vt.T * Sinv).dot(U.T)
+        return self.Gstar
+
+    def step(self):
+        """Advance one step."""
+        field = np.array(self.current_E_fields())
+        if self.need_spatial_weighting:
+            # ellipsis, :, :, newaxis = elementwise product on spatial axes only
+            E = field * self.dh_mask[..., :, :]
+        else:
+            E = field
+        E = field[..., self.binary_dh_mask].ravel()
+        M = self.M
+        Ew = self.Ework
+        Ew[:M] = E.real
+        Ew[M:] = E.imag
+
+        y = self.Gstar.dot(Ew)
+        self.act_cmds -= self.loop_gain * y
+        self.set_actuators(self.act_cmds)
+        self.iter += 1
+        return field
+
+
+class EFC2:
+    """Electric Field Conjugation."""
+    def __init__(self, G, c, beta, dh_mask, wvls, weights, loop_gain=0.5):
         """Create a new Electric Field Conjugation instance.
 
         Parameters
@@ -65,6 +208,9 @@ class EFC:
             the cutoff point used in Tikhonov regularization, the log10 of beta
         dh_mask : numpy.ndarray
             mask which selects and (TODO) optionally weights the dark hole
+        wvls : float or numpy.ndarray
+            wavelengths of light
+        weights : float or numpy.ndarray
         loop_gain : float
             the gain used.  A gain of one half is equivalent to steepest descent
             while a gain of two is equal to steepest descent with a double step
--- dygdug/models.py
+++ dygdug/models.py
@@ -1,27 +1,288 @@
+import inspect
 from pathlib import Path
+from collections import namedtuple
 
 from astropy.io import fits
 
 from prysm import (
     propagation,
     coordinates,
+    polynomials,
     geometry,
     fttools,
 )
 from prysm.experimental.dm import DM
+from prysm._richdata import RichData
 from prysm.mathops import np
 WF = propagation.Wavefront
 
 DEFAULT_IFN_FN = 'influence_dm5v2.fits'
 
+ImgSamplingSpec = namedtuple('ImageSamplingSpec', ['N', 'lamD', 'px_per_lamD'])
+
+
+class WavelengthDependentFunctionCache:
+    def __init__(self, f):
+        self.f = f
+        self.storage = {}
+
+    def clear(self):
+        self.storage = {}
+
+    def __call__(self, wvl):
+        data = self.storage.get(wvl, None)
+        if data is None:
+            data = self.storage[wvl] = self.f(wvl)
+
+        return data
+
+    def nbytes(self):
+        total = 0
+        for v in self.storage.values():
+            total += v.nbytes
+
+        return total
+
+
+def _geometry_factory(func, N, dx, **kwargs):
+    sig = inspect.signature(func)
+    params = set(sig.parameters.keys())
+    xyrt = {'x', 'y', 'r', 't'}
+    # intersection = filter function parameters to just x,y,r,t so we know what to pass
+    need_args = xyrt.intersection(params)
+
+    # reuse name now... |= is a  py 3.9+ dictionary union
+    # (could use kwarg.update(kwargs) for <=3.9 compat)
+    x, y = coordinates.make_xy_grid(N, dx=dx)
+    xyrt = dict(x=x, y=y)
+    if 'r' in need_args or 't' in need_args:
+        r, t = coordinates.cart_to_polar(x, y)  # NOQA: linter blind to the fact that we may access r, t through locals
+        xyrt.update(dict(r=r, t=t))
+    kwarg = {k: xyrt[k] for k in need_args}
+    kwarg.update(kwargs)
+    return func(**kwarg)
+
+
+class Pupil:
+    """Representation of a pupil."""
+    def __init__(self, data, dx):
+        """Create a new Pupil.
+
+        Parameters
+        ----------
+        data : numpy.ndarray
+            2D array, real or complex, containing the pupil
+        dx : float
+            intersample spacing, mm
+
+        """
+        self.data = data
+        self.dx = dx
+        # TODO: if we store this as an int instead of shape, all the prysm
+        # routines will just go back to shape, but if user touches N, maybe
+        # they want an int?  But int forces square, and prysm don't care...
+        self.N = data.shape
+
+    @classmethod
+    def circle(cls, N, Dpup, Npup):
+        dx = Dpup/Npup
+        data = _geometry_factory(geometry.truecircle, N=N, dx=dx, radius=Dpup/2)
+        return cls(data=data, dx=dx)
+
+
+class FPM:
+    """Representation of a focal plane mask."""
+    def __init__(self, func_lam, dx):
+        self.f = func_lam
+        self.dx = dx
+
+    def __call__(self, wvl):
+        return self.f(wvl)
+
+    @classmethod
+    def lyot(cls, N, lamD, px_per_lamD, radius):
+        # lamD has some physical unit (um, say)
+        # px per lamD describes our resolution
+        dx = lamD / px_per_lamD
+        x, y = coordinates.make_xy_grid(N, dx=dx)
+        r = np.hypot(x, y)
+
+        def fpmfunc(wvl):
+            return 1 - geometry.truecircle(radius*lamD, r)
+
+        # TODO: need to del x, y here?  They should be collected because
+        # they went out of scope, but maybe keeping r around through a closure
+        # keeps them too?
+
+        # humor
+        caller_number_1 = WavelengthDependentFunctionCache(fpmfunc)
+        return cls(caller_number_1, dx)
+
+    @classmethod
+    def unity(cls, N, lamD, px_per_lamD):
+        dx = lamD / px_per_lamD
+        ones = np.ones((N, N))
+        def fpmfunc(wvl):
+            return ones
+
+        return cls(fpmfunc, dx)
+
+
+class ThreePlaneSingleDMCoronagraph:
+    def __init__(self, pupil, dm, fpm, ls, imgspec, efl):
+        self.pupil = pupil
+        self.dm = dm
+        self.fpm = fpm
+        self.ls = ls
+        self.imgspec = imgspec
+        self.efl = efl
+        self.dm_wfe = self.dm.render(wfe=True)
+        self.norm = 1
+        self.method = 'mdft'
+
+    def update_all_dms(self, actuators):
+        self.dm.update(actuators)
+        self.dm_wfe = self.dm.render(wfe=True)
+        return [self.dm_wfe]
+
+    def current_actuators(self):
+        acts = self.dm.actuators
+        if self.dm.mask is not None:
+            acts = acts[self.dm.mask]
+
+        return acts.ravel()
+
+    def _fwd_no_coro(self, wvl):
+        """For computing normalization only."""
+        wf = WF.from_amp_and_phase(self.pupil.data, phase=None, wavelength=wvl, dx=self.pupil.dx)
+        img = wf.focus_fixed_sampling(self.efl,
+                                      dx=self.imgspec.lamD/self.imgspec.px_per_lamD,
+                                      samples=self.imgspec.N,
+                                      method=self.method)
+        return img.intensity
+
+    def fwd(self, wvl, norm=True, debug=False):
+        fpm = self.fpm(wvl)
+        wf = WF.from_amp_and_phase(self.pupil.data, phase=self.dm_wfe, wavelength=wvl, dx=self.pupil.dx)
+        after_lyot, at_fpm, after_fpm, at_lyot = \
+            wf.babinet(efl=self.efl, lyot=self.ls.data, fpm=1 - fpm, fpm_dx=self.fpm.dx,
+                       method=self.method, return_more=True)
+
+        img = after_lyot.focus_fixed_sampling(self.efl,
+            dx=self.imgspec.lamD/self.imgspec.px_per_lamD,
+            samples=self.imgspec.N,
+            method=self.method)
+
+        if norm:
+            img.data *= self.norm
+        if debug:
+            return {
+                'input': wf,
+                'fpm': (at_fpm, after_fpm),
+                'lyot': (at_lyot, after_lyot),
+                'img': img,
+            }
+        else:
+            return img.data
+
+    def fwd_bb(self, wvls, weights, debug=False):
+        # TODO logic about adjusting the norm?  As-is user has to set_norm() first,
+        # seems generally OK even if it's a sharp edge?
+
+        # why does radiometry always make me feel autistic......................
+        # know the scale factor so that incoherent sum over fwd(w)*weight
+
+        if weights is not None:
+            weights = weights * self.norm
+        else:
+            weights = [self.norm for _ in range(len(wvls))]
+        if debug:
+            packets = [self.fwd(w, norm=False, debug=True) for w in wvls]
+            for p, w in zip(packets, weights):
+                p['img'].data *= w
+
+            return packets
+        else:
+            fields = [self.fwd(w, norm=False, debug=False) for w in wvls]
+            for f, w in zip(fields, weights):
+                f.data *= w
+
+            return fields
+
+    def set_norm(self, wvls, weights):
+        if isinstance(wvls, float):
+            # monochromatic
+            self.norm = 1 / np.sqrt(self._fwd_no_coro(wvls).data.max())
+            return
+
+        stack = np.array([self._fwd_no_coro(w).data for w in wvls])
+        psf = polynomials.sum_of_2d_modes(stack, weights)
+        self.norm = 1 / np.sqrt(psf.max())
+        # self.norm = 1 / (len(wvls)*np.sqrt(self._fwd_no_coro(.550).data.max()))
+        return
+
+
+def curry_nb(model, wvl):
+    model.set_norm(wvl, 1)
+
+    def paste():
+        return model.fwd(wvl, norm=True)
+
+    return paste
+
+def curry_bb(model, wvls, weights):
+    model.set_norm(wvls, weights)
+
+    def chutney():
+        return model.fwd_bb(wvls, weights)
+
+    return chutney
+
+
+def one_sided_annulus(iss, iwa, owa, azmin, azmax):
+    dx = iss.lamD / iss.px_per_lamD
+    x, y = coordinates.make_xy_grid(iss.N, dx=dx)
+    r, t = coordinates.cart_to_polar(x, y)
+    iwa = iwa * iss.lamD
+    owa = owa * iss.lamD
+    rs = np.radians(azmin)
+    re = np.radians(azmax)
+    mask1 = geometry.circle(iwa, r)
+    mask2 = geometry.circle(owa, r)
+    mask3 = t > rs
+    mask4 = t < re
+    dh_mask = (mask2 ^ mask1) & (mask3 & mask4)
+    return dh_mask
+
+
+def plottable(field, model):
+    if isinstance(field, WF):
+        return field.intensity
+    if isinstance(field, RichData):
+        return field
+
+    if field.ndim == 3:
+        # spectral cube
+        I = incoh_sum(field)
+    else:
+        I = abs(field)**2
+    # diameter = model.imgspec.N / model.imgspec.px_per_lamD
+    return RichData(I, dx=1/model.imgspec.px_per_lamD, wavelength=None)
+
+
+def incoh_sum(es):
+    Is = [abs(e)**2 for e in es]
+    return sum(Is)
+
 
 class LyotCoronagraphSingleDM:
-    def __init__(self, Nmodel, Npup, Nlyot, Nfpm, Nimg, Dpup, fpm_oversampling, image_oversampling, rFPM, wvl0, fno, data_root, ifn_fn, iwa, owa, start_az, end_az):
+    def __init__(self, Nmodel, Npup, Nlyot, Nfpm, Nimg, Dpup, Nact, fpm_oversampling, image_oversampling, rFPM, wvl0, fno, data_root, ifn_fn, iwa, owa, start_az, end_az):
         self.Nmodel = Nmodel
         self.Npup = Npup
         self.Nlyot = Nlyot
         self.Nfpm = Nfpm
         self.Dpup = Dpup
+        self.Nact = Nact
         self.fpm_oversampling = fpm_oversampling
         self.rFPM = rFPM
         self.wvl0 = wvl0
@@ -84,7 +345,7 @@ class LyotCoronagraphSingleDM:
         ifn_sampling_factor = 10  # dimensionless
         ifn_pitch = dm_act_pitch / ifn_sampling_factor  # mm
         dm_angle_deg = (0, 0, 0)
-        nact = 32
+        nact = self.Nact
         dm_diam_mm = dm_act_pitch * nact
         dm_diam_px = dm_diam_mm * ifn_sampling_factor
         dm_model_res = int(dm_diam_px + 4 * ifn_sampling_factor)
